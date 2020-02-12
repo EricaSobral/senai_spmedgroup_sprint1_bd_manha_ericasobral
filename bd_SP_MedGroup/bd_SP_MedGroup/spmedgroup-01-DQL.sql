6679d9a80aec61e3f16693d8b0539650a3f74472
FIC <FIC@info.local>
2020-02-12 11:10:44 -0200

    Fina

--- bd_SP_MedGroup/bd_SP_MedGroup/spmedgroup-01-DQL.sql
+++ bd_SP_MedGroup/bd_SP_MedGroup/spmedgroup-01-DQL.sql
@@ -3,6 +3,13 @@
  -- Quantidade de Usuarios
  SELECT COUNT (id_usuario) as Total_Usuario FROM Usuario
 
- --
+ --  Medico e sua especialidade
 
+CREATE PROCEDURE Numero
+@Quantidade INT 
+AS
+SELECT Especialidades.Especialidade, Medicos.Nome_medico FROM Medicos
+INNER JOIN Especialidades ON Especialidades.id_especialidade = Medicos.fk_especialidade
+WHERE id_medico = @Quantidade;
 
+EXECUTE Numero 1;
